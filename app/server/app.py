9abce7f62129c5b4b9e807ce99e86c3b1da2838b
Ankit Hans <ankithans1947@gmail.com>
2021-02-04 16:35:39 +0530

    feat: include student router in app

--- app/server/app.py
+++ app/server/app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from server.routes.student import router as StudentRouter
 
 app = FastAPI()
+
+app.include_router(StudentRouterm tags=["Students"], prefix="/student")
 
 @app.get("/", tags=["Root"])
 async def read_root():
